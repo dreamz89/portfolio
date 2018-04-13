dbbb70711dec3a12cb55062e95004795294e1126
Jocelyn Tan <jocelyntan89@hotmail.com>
2018-04-13 16:12:14 +0800

    added skills section and mac picture on top section

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ navlink.on('click',function(){
   navlink.css('color','black');
   $(this).css('color','#005E5E');
 });
+
+var texts = ['user experience', 'interaction design', 'responsive design'];
+var changing = $('#changing');
+var counter = 0;
+var t = setInterval(function(){
+  changing.html(texts[counter]);
+  counter++;
+  if (counter >= texts.length) {
+    counter = 0;
+  }
+}, 2500);
