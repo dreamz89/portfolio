26f8afcd6044d70d27baa8c6440e7feb2a745060
Jocelyn Tan <jocelyntan89@hotmail.com>
2018-04-24 19:19:44 +0800

    removed jquery library for optimization

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-var navlink = $('.topnav-right a');
-navlink.on('click',function(){
-  navlink.css('color','black');
-  $(this).css('color','#005E5E');
-});
+var navlinks = document.querySelectorAll('.topnav-right a');
+navlinks.forEach(function(navlink){
+  navlink.addEventListener("click", function(){
+    navlinks.forEach(function(navlink){
+      navlink.style.color = 'black';
+    })
+    navlink.style.color = '#005E5E';
+  });
+})
 
 var texts = ['user experience', 'interaction design', 'responsive design'];
-var changing = $('#changing');
+var changing = document.querySelector('#changing');
 var counter = 0;
 var t = setInterval(function(){
-  changing.html(texts[counter]);
+  changing.innerHTML = texts[counter];
   counter++;
   if (counter >= texts.length) {
     counter = 0;
